10cb8079d564816bc246ae30b6004fd75b54c4a8
grace-th3 <dummy@fake.com>
2021-10-19 10:32:28 +1100

    hello

---
+++ goodbyeworld.py
@@ -0,0 +1,11 @@
+
+print (" ---------------------------------------------- ")
+print ("|                                              |")
+print ("|    Hello World                               |")
+print ("|    Name : Grace Thomas                       |")
+print ("|    Version : 01                              |")
+print ("|                                              |")
+print (" ---------------------------------------------- ")
+print ("Hello World")
+input ("Program paused - press enter to continue...")
+print("Goodbye World")
