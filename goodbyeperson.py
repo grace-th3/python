32c2bc06d0453be706a8e36c83ea0c98490ad6ae
grace-th3 <dummy@fake.com>
2021-10-19 11:29:52 +1100

    hello3

---
+++ goodbyeperson.py
@@ -0,0 +1,12 @@
+
+print (" ---------------------------------------------- ")
+print ("|                                              |")
+print ("|    03GoodbyePerson                           |")
+print ("|    Name : Grace Thomas                       |")
+print ("|    Version : 01                              |")
+print ("|                                              |")
+print (" ---------------------------------------------- ")
+print ("Hello")
+value = input("What is your name ? ") 
+print (f'Goodbye {value}')
+
