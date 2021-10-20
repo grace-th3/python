77273e973103c0622174db4cef37bbf6ca182cf0
grace-th3 <dummy@fake.com>
2021-10-20 12:31:19 +1100

    hello3

---
+++ goodteacher.py
@@ -0,0 +1,15 @@
+ 
+print (" ---------------------------------------------- ")
+print ("|                                              |")
+print ("|    04GoodTeacher                             |")
+print ("|    Name : Grace Thomas                       |")
+print ("|    Version : 01                              |")
+print ("|                                              |")
+print (" ---------------------------------------------- ")
+
+
+value = input("Teacher's name (try Mr Horan) ")
+if value == "Mr Horan":
+    print("You are lucky, he is a great teacher") 
+else:
+    print(value, "is an ok teacher")
