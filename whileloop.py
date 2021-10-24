b9da3ec6e31b627908059aa1aa39299c9cac88c3
grace-th3 <dummy@fake.com>
2021-10-25 10:57:52 +1100

    HI

---
+++ whileloop.py
@@ -0,0 +1,29 @@
+ 
+print (" ---------------------------------------------- ")
+print ("|                                              |")
+print ("|    06whileLoop                               |")
+print ("|    Name : Grace Thomas                       |")
+print ("|    Version : 01                              |")
+print ("|                                              |")
+print (" ---------------------------------------------- ")
+
+
+value = input("What is the name of this subject? " )
+if value == "IST": 
+    print("               ")
+    print("               ")
+    print(" Congratulations!!")
+    print("               ")
+    print("               ")
+    print("               ")
+else:
+    print("Not Correct - try again")
+    
+value = input("What is the name of this subject? " )
+if value == "IST": 
+    print("               ")
+    print("               ")
+    print(" Congratulations!!")
+    print("               ")
+    print("               ")
+    print("               ")
