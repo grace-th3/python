d045cf1e501f6ba84b9308ab6708af2603d5204e
grace-th3 <dummy@fake.com>
2021-11-03 12:19:38 +1100

    menu2

--- 07menu.py
+++ 07menu.py
@@ -103,7 +103,7 @@ def encodestring():
         print ("                       ")  
         print ("                       ")
 
-def encodestring():
+def end():
         print ("                       ")
         print ("----Start of Output ---------------------------")
         print ("                       ")
@@ -258,8 +258,12 @@ if value == "9":
         input("Press Enter to continue")
         clear()
 if value == "x": 
-        encodestring()
+        end()
         input("Press Enter to continue")
+
+if value != ("1") and ("2")and ("3")and ("4")and ("5")and ("6")and ("7")and ("8")and ("9")and ("x"):
+        encodestring()
+
 
 
 
